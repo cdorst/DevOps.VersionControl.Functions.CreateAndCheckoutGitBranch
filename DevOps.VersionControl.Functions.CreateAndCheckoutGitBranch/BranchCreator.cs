04330a0561e963ee7f406b9e64277e41366b237b
Christopher Dorst <chris@dorst.email>
2018-01-02 04:25:09 -0600

    Add BranchCreator

---
+++ DevOps.VersionControl.Functions.CreateAndCheckoutGitBranch/BranchCreator.cs
@@ -0,0 +1,14 @@
+﻿using static DevOps.VersionControl.Functions.RunGitBranchCommand.GitBranchCommandRunner;
+using static DevOps.VersionControl.Functions.RunGitCheckoutCommand.GitCheckoutCommandRunner;
+
+namespace DevOps.VersionControl.Functions.CreateAndCheckoutGitBranch
+{
+    public static class BranchCreator
+    {
+        public static void BranchAndCheckout(string directory, string name)
+        {
+            Branch(directory, name);
+            Checkout(directory, name);
+        }
+    }
+}
